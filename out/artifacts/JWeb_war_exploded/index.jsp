58530e8a0adda1288085a86cc7ac3e3e38b29f90
Maxime Menigoz <maxime.menigoz@gmail.com>
2015-12-09 11:52:54 +0100

    change IDE

---
+++ out/artifacts/JWeb_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: menigo_m
+  Date: 08/12/15
+  Time: 19:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>JWeb</title>
+  </head>
+  <body>
+  <h1>Test</h1>
+  </body>
+</html>
---
+++ src/com/jweb/servlet/Servlet.java
@@ -0,0 +1,20 @@
+package com.jweb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by menigo_m on 08/12/15.
+ */
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
