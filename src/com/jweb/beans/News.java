9146fb321cb3e89b12752a7dcde286d8707384d8
Maxime Menigoz <maxime.menigoz@gmail.com>
2015-12-16 14:20:41 +0100

    add jstl

---
+++ src/com/jweb/beans/News.java
@@ -0,0 +1,7 @@
+package com.jweb.beans;
+
+/**
+ * Created by menigo_m on 15/12/15.
+ */
+public class News {
+}
--- src/com/jweb/servlet/Servlet.java
+++ src/com/jweb/servlet/Servlet.java
@@ -1,21 +1,20 @@
 package com.jweb.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by menigo_m on 08/12/15.
  */
 public class Servlet extends HttpServlet {
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html; charset=UTF-8");
-        this.getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String msg = "Voici un element passé du servlet index à la vue";
+        request.setAttribute("example", msg);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
